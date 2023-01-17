d8348a6c855342442a3a4566c734fb72a366f163
wajeht <58354193+wajeht@users.noreply.github.com>
2023-01-17 01:54:07 -0600

    chore: updated git cache

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import compression from 'compression';
 import helmet from 'helmet';
 import express from 'express';
 
-import * as appRoutes from './app.routes.js';
+import * as appMiddlewares from './app.middlewares.js';
 import apiRoutes from './api/api.js';
 
 const app = express();
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(path.join(process.cwd(), 'public'))));
 
 app.use('/api', apiRoutes);
-app.use(appRoutes.notFoundApiHandler);
-app.use(appRoutes.errorApiHandler);
-app.use('*', appRoutes.reactHandler);
+app.use(appMiddlewares.notFoundApiHandler);
+app.use(appMiddlewares.errorApiHandler);
+app.use('*', appMiddlewares.reactHandler);
+app.use(appMiddlewares.notFoundApiHandler);
+app.use(appMiddlewares.errorApiHandler);
 
 export default app;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Users from './components/Users';
+import mariposa from './assets/mariposa.jpeg';
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -8,7 +9,7 @@ export default function App() {
     <div className="h-screen flex flex-col justify-center items-center gap-5 bg-neutral-200">
       <p>Edit src/App.jsx and save to test</p>
       <div className="bg-neutral-50 p-5 rounded-md text-center flex flex-col gap-3">
-        <img src="./assets/mariposa.jpeg" className="mx-auto rounded" alt="mariposa logo" />
+        <img src={mariposa} className="mx-auto rounded" alt="mariposa logo" />
         <h1 className="text-3xl font-bold">Mariposa Tech Hub</h1>
         <button
           className="bg-slate-500 hover:bg-slate-700 text-slate-100 p-2 rounded"
