c2ea3ee4da2f3ac5f31f6662f502135a3a6c477b
wajeht <58354193+wajeht@users.noreply.github.com>
2023-01-18 01:21:06 -0600

    feat: scaffoled blank pages

--- src/api/api.errors.js
+++ src/api/api.errors.js
@@ -3,7 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 class CustomAPIError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'CustomAPIError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
--- src/api/api.routes.js
+++ src/api/api.routes.js
@@ -2,7 +2,9 @@ import express from 'express';
 const api = express.Router();
 
 import userRoutes from './users/user.routes.js';
+import authRoutes from './auth/auth.routes.js';
 
 api.use('/users', userRoutes);
+api.use('/auth', authRoutes);
 
 export default api;
