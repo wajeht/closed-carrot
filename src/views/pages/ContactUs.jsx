990465b55e64d5b675d91e8209015d763f7bfa97
wajeht <58354193+wajeht@users.noreply.github.com>
2023-01-20 20:06:05 -0600

    chore: scaffolded more pages

---
+++ src/views/pages/ContactUs.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function ContactUs() {
+  return <div>Contact us</div>;
+}
---
+++ src/views/pages/PrivacyPolicy.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function PrivacyPolicy() {
+  return <div>Privacy policy</div>;
+}
