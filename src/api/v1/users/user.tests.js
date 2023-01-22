0e59c1cef00824b8f30674326b53da2b9398add0
wajeht <58354193+wajeht@users.noreply.github.com>
2023-01-21 22:50:33 -0600

    fix: app test was not correct path

--- src/api/v1/users/user.tests.js
+++ src/api/v1/users/user.tests.js
@@ -4,9 +4,9 @@ import { StatusCodes } from 'http-status-codes';
 
 import app from '../../../app.js';
 
-describe('GET /api/users', () => {
+describe('GET /api/v1/users', () => {
   it('should returns a success response', async () => {
-    const res = await request(app).get('/api/users');
+    const res = await request(app).get('/api/v1/users');
     expect(res.statusCode).toBe(StatusCodes.OK);
   });
 });
