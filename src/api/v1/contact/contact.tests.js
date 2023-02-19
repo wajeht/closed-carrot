70d69de5113817db1ea991ae226fd6c6d34ef600
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-19 03:09:53 -0600

    feat: fixed contact tests

---
+++ src/api/v1/contact/contact.tests.js
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { describe, it, expect } from 'vitest';
+import { StatusCodes } from 'http-status-codes';
+import { faker } from '@faker-js/faker';
+
+import app from '../../../app.js';
+
+describe('POST /api/v1/contact', () => {
+  it('should returns a success response when sending a contact request with all body fields', async () => {
+    const res = await request(app).post('/api/v1/contact').send({
+      email: faker.internet.email(),
+      subject: faker.datatype.string(),
+      message: faker.datatype.string(),
+    });
+    expect(res.body.message).toBe('your message has been sent successfully!');
+    expect(res.statusCode).toBe(StatusCodes.OK);
+  });
+
+  it('should returns a validation error response when sending a contact request with some body fields', async () => {
+    const res = await request(app).post('/api/v1/contact').send({
+      email: faker.internet.email(),
+      message: faker.datatype.string(),
+    });
+    expect(res.statusCode).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
+  });
+});
