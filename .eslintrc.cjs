2a10ddd64030dc88e093873a5c88420ce6ad4e1d
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-20 10:30:54 -0600

    feat: more eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -12,13 +26,6 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'react/prop-types': 'off',
-  },
   settings: {
     react: {
       version: 'detect',
