b0d55824ed2dc7dc95bb13d90ea23b4066e95459
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2023-02-25 17:56:04 -0600

    images
    headshots

--- src/views/components/Header.jsx
+++ src/views/components/Header.jsx
@@ -1,35 +1,65 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import { Link } from 'react-router-dom';
-=======
 import { Link, useLocation } from 'react-router-dom';
-import Logo from '../assets/Images/Closed Carrot Logo_Icon_Transparent.png';
->>>>>>> Stashed changes
+import Logo from '../assets/closed-carrot-logo.png';
 
 export default function Header() {
+  const location = useLocation();
+
   return (
-    <header className="bg-slate-300 p-3 shadow-md">
-      <div className="max-w-7xl mx-auto flex justify-between">
+    <header className="bg-white h-[100px] p-3">
+      <div className="max-w-7xl mx-auto flex justify-between min-h-full items-center">
         <h1>
-          <Link to="/">🥕 Closed Carrot: Amarillo&apos;s Tech Hub</Link>
+          <Link to="/">
+            <img className="max-h-[35px]" src={Logo} alt="closed carrot logo" />
+          </Link>
         </h1>
+
         <nav>
-          <ul className="flex gap-3">
-            <li>
-              <Link to="/">Home</Link>
+          <ul className="flex gap-12">
+            <li className="max-w-xs">
+              <Link
+                className={
+                  location.pathname === '/' ? 'underline decoration-4 decoration-orange-400' : null
+                }
+                to="/"
+              >
+                HOME
+              </Link>
             </li>
-            <li>
+
+            {/* <li>
               <Link to="/users">Users</Link>
-            </li>
+            </li> */}
+
             <li>
-              <Link to="/about-us">About Us</Link>
+              <Link
+                className={
+                  location.pathname === '/about-us'
+                    ? 'underline decoration-4 decoration-orange-400'
+                    : null
+                }
+                to="/about-us"
+              >
+                ABOUT US
+              </Link>
             </li>
+
             <li>
-              <Link to="/contact-us">Contact Us</Link>
+              <Link
+                className={
+                  location.pathname === '/contact-us'
+                    ? 'underline decoration-4 decoration-orange-400'
+                    : null
+                }
+                to="/contact-us"
+              >
+                CONTACT US
+              </Link>
             </li>
-            <li>
+
+            {/* <li>
               <Link to="/login">Login</Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
       </div>
