49fe54a53c13b8b6dbf544e67969ec9f71002b79
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-26 17:37:47 -0600

    fix: images sources were not building on vite

---
+++ src/views/data/images.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    name: 'group-around-computer',
+    url: '../assets/images/marketing/group-around-computer.png',
+  },
+  {
+    name: 'group-around-whiteboard',
+    url: '../assets/images/marketing/group-around-whiteboard.png',
+  },
+  {
+    name: 'orange-coffee-cup',
+    url: '../assets/images/marketing/orange-coffee-cup.png',
+  },
+  {
+    name: 'podcast-man-and-woman',
+    url: '../assets/images/marketing/podcast-man-and-woman.png',
+  },
+  {
+    name: 'racially-ambiguous-hands-on-keyboard',
+    url: '../assets/images/marketing/racially-ambiguous-hands-on-keyboard.png',
+  },
+  {
+    name: 'supplies-on-desk',
+    url: '../assets/images/marketing/supplies-on-desk.png',
+  },
+  {
+    name: 'woman-on-computer',
+    url: '../assets/images/marketing/woman-on-computer.png',
+  },
+  {
+    name: 'woman-on-ipad',
+    url: '../assets/images/marketing/woman-on-ipad.png',
+  },
+];
