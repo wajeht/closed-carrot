24cbebff414f68886dc6270a76493a4d4f483dd0
wajeht <58354193+wajeht@users.noreply.github.com>
2023-02-26 20:38:24 -0600

    fix: url

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 
 source .env
 
-npm run build:ui
-
 caprover deploy -h $CAPROVER_DOMAIN -p $CAPROVER_PASSWORD -b $CAPROVER_GIT_BRANCH_NAME -a $CAPROVER_APP_NAME
--- src/views/data/users.js
+++ src/views/data/users.js
@@ -1,38 +1,45 @@
+import jerry from '../assets/images/headshots/jglover.jpg';
+import mcall from '../assets/images/headshots/mccall.jpg';
+import will from '../assets/images/headshots/will.jpeg';
+import karl from '../assets/images/headshots/KarlShinn.jpg';
+import david from '../assets/images/headshots/davidwilson2.png';
+import jermy from '../assets/images/headshots/jeremy.png';
+
 export default [
   {
     name: 'Jerry',
     title: 'Project Manager',
-    image: '../assets/images/headshots/jglover.jpg',
+    image: jerry,
     bio: 'Jerri serves Closed Carrot as our business development and project management lead. She has a diverse professional background including mediation, real estate, project management, small business owner, court reporter legal assistant, training and even worked on the Space Shuttle Recovery team at Edward Air Force Base in California. She is passionate about community and helping others excel in their personal and professional goals. ',
   },
   {
     name: 'Mccall',
     title: 'Head of Brand',
-    image: '../assets/images/headshots/mccall.jpg',
+    image: mcall,
     bio: 'McCall Money double majored in Public Relations and Advertising at The University of Texas at Arlington. While in college, McCall was a member of the UT Arlington chapter of the American Advertising Federation, Public Relations Student Society of America, and led many of her projects as the Campaign Manager. McCall has experience with the Texas Rangers Baseball Club in customer service and promotions, and has also served as the Promotions Coordinator for Chick-fil-A. McCall’s mission is to provide clients with first-class care and attention. She is passionate about creating a seamless user experience and crafting innovative ideas through marketing, photography, and web design. ',
   },
   {
     name: 'Will',
     title: 'Full-stack Developer',
-    image: '../assets/images/headshots/will.jpeg',
+    image: will,
     bio: 'William Manning is an IT Business Analyst and Project Coordinator with experience in software development, reporting, automation, and project management. He has worked on custom software using MS Access, Crystal Reporting, SQL, and VBA, and has developed automation processes for Mulesoft Developers. William is proficient in various front-end and back-end programming languages, such as JavaScript, React, Python, and SQL. He is skilled in project planning, risk management, cost estimation, and schedule reporting, and has experience in maintaining data integrity and configuration control. Additionally, William has a Bachelors degree in Business Management from West Texas A&M University, and an Associates degree in Business Administration from Amarillo College. ',
   },
   {
     name: 'Karl',
     title: 'Full-stack Developer',
-    image: '../assets/images/headshots/KarlShinn.jpg',
+    image: karl,
     bio: 'Karl is a Regional Lead for a tech company specializing in green data centers. He has expertise in various aspects of tech, including full stack web development, User Interface development, automation, blockchain, and project management. His programming competencies include Javascript, HTML/CSS, React, Svelte, and is adept at working with SQL and NoSQL databases. Prior to his work in tech development, Karl had over 13 years of military and Federal service, where he employed various technologies within traditional and special operations.',
   },
   {
     name: 'David',
     title: 'Front-end Developer',
-    image: '../assets/images/headshots/davidwilson2.png',
+    image: david,
     bio: 'David possesses a Bachelor degree in Psychology with a minor in Fine Arts from Eastern New Mexico University, a Master of Arts in Postsecondary Education from San Diego State University, a Master of Education in Instructional Design & Technology from West Texas A&M University, as well as a certificate in Full Stack Web Development from Amarillo College. His career background is in education, digital marketing, technology, and web design. Some of his passions include photography, videography, graphic design, and hot air ballooning. In terms of web development, David’s focus has always been on the user experience and usability; he loves building responsive websites that are aesthetically pleasing and easy to use.',
   },
   {
     name: 'Jeremy',
     title: 'Designer',
-    image: '../assets/images/headshots/jeremy.png',
+    image: jermy,
     bio: 'Jeremy is an aspiring new Software Developer for the Carrot. He is proficient in building computers, Graphic design, fundamentals of a Twitch/YouTube streamer, photography, lighting design, webpage design and audio engineering. Fun fact- Jeremy may be the only known man to allow a baby Yak to attempt to suck milk from his fingers!  In conclusion, whether you are looking to stay on top of the competition or you don’t even know how to open your email, Jeremy will take your business to new heights.',
   },
   {
