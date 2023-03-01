215d19b78d118a9c59d016e1e84ed0703a310fe4
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2023-02-28 21:07:41 -0600

    users
    added users

--- src/views/components/CoreValues.jsx
+++ src/views/components/CoreValues.jsx
@@ -17,10 +17,15 @@ export default function CoreValues() {
             <div>
               <h2 className="font-bold text-xl text-center mb-2">Our Mission</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, corporis
-                accusamus. Itaque voluptatum repellendus quisquam excepturi possimus ipsam ea
-                commodi numquam, perspiciatis neque aliquid facere velit adipisci blanditiis quod
-                eaque.
+                Our mission at the Closed Carrot is to provide students in Amarillo, Texas with the
+                opportunity to gain hands-on experience in the tech industry through collaborative
+                and innovative projects. As a student-led organization, we strive to foster an
+                inclusive community of individuals who are passionate about technology and who are
+                committed to developing their skills through teamwork, mentorship, and professional
+                development opportunities. Our goal is to create a supportive and engaging
+                environment where students can explore their interests, pursue their passions, and
+                develop the skills and knowledge they need to succeed in the rapidly evolving tech
+                industry.
               </p>
             </div>
           </div>
@@ -30,10 +35,14 @@ export default function CoreValues() {
             <div>
               <h2 className="font-bold text-xl text-center mb-2">Our Plan</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, corporis
-                accusamus. Itaque voluptatum repellendus quisquam excepturi possimus ipsam ea
-                commodi numquam, perspiciatis neque aliquid facere velit adipisci blanditiis quod
-                eaque.
+                Our intention at the is to empower students in Amarillo, Texas with the skills,
+                knowledge, and opportunities they need to thrive in the tech industry. By providing
+                a supportive and collaborative environment, we aim to inspire students to pursue
+                their interests and passions in technology, to learn from each other and from
+                experienced mentors, and to build a community that values innovation, creativity,
+                and continuous learning. Through our projects, events, and initiatives, we strive to
+                create a culture of growth, competence, and stability, and to contribute to the
+                growth and success of the tech industry in Amarillo and beyond.
               </p>
             </div>
           </div>
@@ -43,10 +52,14 @@ export default function CoreValues() {
             <div>
               <h2 className="font-bold text-xl text-center mb-2">Our Vision</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, corporis
-                accusamus. Itaque voluptatum repellendus quisquam excepturi possimus ipsam ea
-                commodi numquam, perspiciatis neque aliquid facere velit adipisci blanditiis quod
-                eaque.
+                Our vision at the Amarillo Tech Co-op is to be a driving force in the development of
+                a thriving and innovative tech ecosystem in Amarillo, Texas. We envision a future
+                where students have access to the resources, opportunities, and support they need to
+                pursue their dreams and build successful careers in the tech industry. We aspire to
+                create a community that fosters lifelong learning, creativity, and collaboration,
+                and that values speed, surprise, and violence of action. Our ultimate goal is to
+                inspire and empower the next generation of tech leaders who will shape the future of
+                Amarillo and beyond.
               </p>
             </div>
           </div>
