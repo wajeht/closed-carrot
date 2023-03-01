f25dc233662321f9c30ec6260da7c8c0615bdd68
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-01 09:35:16 -0600

    fix: images sources were not linking

--- src/views/data/users.js
+++ src/views/data/users.js
@@ -1,63 +1,66 @@
-import jerry from '../assets/images/headshots/jglover.jpg';
+import netty from '../assets/images/headshots/netty.jpeg';
+import gerado from '../assets/images/headshots/gerardo.jpg';
 import mcall from '../assets/images/headshots/mccall.jpg';
 import will from '../assets/images/headshots/will.jpeg';
 import karl from '../assets/images/headshots/KarlShinn.jpg';
 import david from '../assets/images/headshots/davidwilson2.png';
 import jermy from '../assets/images/headshots/jeremy.png';
+import jglover from '../assets/images/headshots/jglover.jpg';
+import kyaw from '../assets/images/headshots/jaw.jpeg';
 
 export default [
   {
     name: 'Jerry',
     title: 'Project Manager',
-    image: '../assets/Images/Headshots/jglover.jpg',
+    image: jglover,
     bio: 'Jerri serves Closed Carrot as our business development and project management lead. She has a diverse professional background including mediation, real estate, project management, small business owner, court reporter legal assistant, training and even worked on the Space Shuttle Recovery team at Edward Air Force Base in California. She is passionate about community and helping others excel in their personal and professional goals. ',
   },
   {
     name: 'Mccall',
     title: 'Head of Brand',
-    image: '../assets/Images/Headshots/mccall.jpg',
+    image: mcall,
     bio: 'McCall Money double majored in Public Relations and Advertising at The University of Texas at Arlington. While in college, McCall was a member of the UT Arlington chapter of the American Advertising Federation, Public Relations Student Society of America, and led many of her projects as the Campaign Manager. McCall has experience with the Texas Rangers Baseball Club in customer service and promotions, and has also served as the Promotions Coordinator for Chick-fil-A. McCall’s mission is to provide clients with first-class care and attention. She is passionate about creating a seamless user experience and crafting innovative ideas through marketing, photography, and web design. ',
   },
   {
     name: 'Will',
     title: 'Full-stack Developer',
-    image: '../assets/Images/Headshots/will.jpeg',
+    image: will,
     bio: 'William Manning is an IT Business Analyst and Project Coordinator with experience in software development, reporting, automation, and project management. He has worked on custom software using MS Access, Crystal Reporting, SQL, and VBA, and has developed automation processes for Mulesoft Developers. William is proficient in various front-end and back-end programming languages, such as JavaScript, React, Python, and SQL. He is skilled in project planning, risk management, cost estimation, and schedule reporting, and has experience in maintaining data integrity and configuration control. Additionally, William has a Bachelors degree in Business Management from West Texas A&M University, and an Associates degree in Business Administration from Amarillo College. ',
   },
   {
     name: 'Karl',
     title: 'Full-stack Developer',
-    image: '../assets/Images/Headshots/KarlShinn.jpg',
+    image: karl,
     bio: 'Karl is a Regional Lead for a tech company specializing in green data centers. He has expertise in various aspects of tech, including full stack web development, User Interface development, automation, blockchain, and project management. His programming competencies include Javascript, HTML/CSS, React, Svelte, and is adept at working with SQL and NoSQL databases. Prior to his work in tech development, Karl had over 13 years of military and Federal service, where he employed various technologies within traditional and special operations.',
   },
   {
     name: 'David',
     title: 'Front-end Developer',
-    image: '../assets/Images/Headshots/davidwilson2.png',
+    image: david,
     bio: 'David possesses a Bachelor degree in Psychology with a minor in Fine Arts from Eastern New Mexico University, a Master of Arts in Postsecondary Education from San Diego State University, a Master of Education in Instructional Design & Technology from West Texas A&M University, as well as a certificate in Full Stack Web Development from Amarillo College. His career background is in education, digital marketing, technology, and web design. Some of his passions include photography, videography, graphic design, and hot air ballooning. In terms of web development, David’s focus has always been on the user experience and usability; he loves building responsive websites that are aesthetically pleasing and easy to use.',
   },
   {
     name: 'Jeremy',
     title: 'Designer',
-    image: '../assets/Images/Headshots/jeremy.png',
+    image: jermy,
     bio: 'Jeremy is an aspiring new Software Developer for the Carrot. He is proficient in building computers, Graphic design, fundamentals of a Twitch/YouTube streamer, photography, lighting design, webpage design and audio engineering. Fun fact- Jeremy may be the only known man to allow a baby Yak to attempt to suck milk from his fingers!  In conclusion, whether you are looking to stay on top of the competition or you don’t even know how to open your email, Jeremy will take your business to new heights.',
   },
   {
     name: 'Kyaw ',
     title: 'Full-stack Developer',
-    image: '../assets/Images/Headshots/jaw.jpeg',
+    image: kyaw,
     bio: 'Kyaw a full-stack web developer, he is passionate about creating dynamic and engaging web applications that deliver value and enhance user experience. Possessing strong technical skills and knowledge in both front-end and back-end development, he has the ability to design and implement robust and scalable solutions that meet business requirements and exceed user expectations. He is skilled in using a range of programming languages and frameworks, including HTML, CSS, JavaScript, Node.js, React, and Angular, and is committed to staying up-to-date with the latest industry trends and technologies. He is a creative problem-solver who enjoys tackling complex challenges and finding elegant solutions that optimize performance, security, and accessibility. With excellent communication, collaboration, and project management skills, he is able to work effectively both independently and as part of a team, and is dedicated to delivering high-quality work that meets and exceeds client expectations.',
   },
   {
     name: 'Netty',
     title: 'Assistant Project Manager',
-    image: '../assets/Images/Headshots/netty.jpeg',
+    image: netty,
     bio: 'Netty is an experienced assistant project manager with strong expertise in Excel, I have a proven track record of success in managing and coordinating projects, analyzing data, and providing insights to support decision-making. I am skilled in developing project plans, timelines, and budgets, and in tracking progress against established goals and objectives. With my advanced proficiency in Excel, I am able to manipulate and analyze large data sets, create dynamic charts and graphs, and generate reports that effectively communicate project status and performance. I am a highly organized and detail-oriented individual with excellent communication, problem-solving, and interpersonal skills, and I thrive in fast-paced, deadline-driven environments. I am committed to delivering high-quality work and to collaborating with stakeholders across teams and functions to achieve project success',
   },
   {
     name: 'Gerardo',
     title: 'Full-stack Developer',
-    image: '../assets/Images/Headshots/gerardo.jpg',
+    image: gerado,
     bio: 'Gerardo is the Lead Software Engineer for a tech startup that develops software for private security firms. He obtained an Associate’s Degree in Engineering Computer Science from Amarillo College, and continued his Computer Science Major at West Texas A&M University. Gerardo switched career goals to pursue his passion of helping others and served as a Catholic Missionary in the Central American country of Nicaragua for ten years. He returned to Texas in 2020 and has since then worked as a Full Stack Web Developer. Gerardo’s expertise is in Backend Web Development specializing in MERN Stack applications, API design, SQL & Mongo Database development, Cloud Micro-Services and Infrastructure, and CI/CD.    ',
   },
 ];
