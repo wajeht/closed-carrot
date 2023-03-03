ddb73d6a5cb00773e0973ad0fd6e665d1a1e3ddf
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-02 20:38:36 -0600

    feat: intregration test for `/api/v1/contact`

--- src/api/v1/contact/contact.tests.js
+++ src/api/v1/contact/contact.tests.js
@@ -1,34 +1,29 @@
 import request from 'supertest';
-import {
-  describe,
-  it,
-  expect,
-  // beforeAll, afterAll
-} from 'vitest';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import { StatusCodes } from 'http-status-codes';
 import { faker } from '@faker-js/faker';
 
 import app from '../../../app.js';
-// import { connectDB, disconnectDB } from '../../../utils/db-test.js';
+import { connectDatabase, closeDatabase } from '../../../utils/db-test.js';
+
+beforeAll(() => {
+  connectDatabase();
+});
+
+afterAll(() => {
+  closeDatabase();
+});
 
 describe('POST /api/v1/contact', () => {
-  //   beforeAll(() => {
-  //     connectDB();
-  //   });
-
-  //   afterAll(() => {
-  //     disconnectDB();
-  //   });
-
-  // it('should returns a success response when sending a contact request with all body fields', async () => {
-  //   const res = await request(app).post('/api/v1/contact').send({
-  //     email: faker.internet.email(),
-  //     subject: faker.datatype.string(),
-  //     message: faker.datatype.string(),
-  //   });
-  //   expect(res.body.message).toBe('your message has been sent successfully!');
-  //   expect(res.statusCode).toBe(StatusCodes.OK);
-  // });
+  it('should returns a success response when sending a contact request with all body fields', async () => {
+    const res = await request(app).post('/api/v1/contact').send({
+      email: faker.internet.email(),
+      subject: faker.datatype.string(),
+      message: faker.datatype.string(),
+    });
+    expect(res.body.message).toBe('your message has been sent successfully!');
+    expect(res.statusCode).toBe(StatusCodes.OK);
+  });
 
   it('should returns a validation error response when sending a contact request with some body fields', async () => {
     const res = await request(app).post('/api/v1/contact').send({
