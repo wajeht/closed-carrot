e3341281450e27b557abb52bb992eb4be15c2f23
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-04 17:38:59 -0600

    fix: hero container widget was big on mobile

--- src/views/components/Hero.jsx
+++ src/views/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="w-full bg-white-board bg-cover bg-center bg-[#FF8B46]">
       <div className="w-full flex flex-col gap-5 justify-center items-center bg-black/50 backdrop-brightness-75">
         <div className="flex justify-center items-center flex-col min-h-[300px] md:min-h-[500px]">
-          <span className="text-white font-bold md:text-4xl text-xl mb-2 mx-1 text-center">
+          <span className="text-white font-bold w-8/12  md:w-full md:text-4xl text-xl mb-2 mx-1 text-center">
             <span>
               <span>Unlock your digital potential with</span>
               <span className="px-1"></span>
