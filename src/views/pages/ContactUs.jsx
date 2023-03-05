1c97e962c60f75ae7882e45b24e27d84dc17b937
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-05 15:52:49 -0600

    fix: typo

--- src/views/pages/ContactUs.jsx
+++ src/views/pages/ContactUs.jsx
@@ -93,7 +93,7 @@ export default function ContactUs() {
         title="closed carrot google map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3260.147988763759!2d-101.8322489838222!3d35.202782580308494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87014f1d7b43cd63%3A0xf5e6505d8ef1da3d!2s701%20SE%2010th%20Ave%2C%20Amarillo%2C%20TX%2079101!5e0!3m2!1sen!2sus!4v1677884091234!5m2!1sen!2sus"
         style={{ border: 0, minHeight: '500px', minWidth: '100%' }}
-        allowfullscreen={true}
+        allowFullScreen={true}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
