1b81f99bcba99e1fb4073817b1d06c2301c415ee
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-07 18:00:53 -0600

    feat: 404 page style chagnes

--- src/views/pages/NotFound.jsx
+++ src/views/pages/NotFound.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <div>this is a not found page</div>;
+  return (
+    <div className="mx-auto w-full max-w-7xl px-3 py-10">
+      <h2 className="text-2xl font-bold text-center underline decoration-3 decoration-orange-500 underline-offset-4">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;404&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </h2>
+      <br />
+      <br />
+      <p className="text-center">The resource requested could not be found on this server!</p>
+    </div>
+  );
 }
