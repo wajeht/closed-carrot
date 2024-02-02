49009b0d89cfaa172418fad0405b05f21762b3b0
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-02 17:47:47 -0600

    chore: remove contact

--- src/api/v1/contact/contact.controllers.js
+++ src/api/v1/contact/contact.controllers.js
@@ -1,7 +1,7 @@
-import Mail from '../../../utils/mail.js';
+// import Mail from '../../../utils/mail.js';
 import { saveContact } from './contact.services.js';
 import Discord from '../../../utils/discord.js';
-import { EMAIL } from '../../../config/constants.js';
+// import { EMAIL } from '../../../config/constants.js';
 
 export async function postContact(req, res) {
   const { email, name, subject, message, forBearsOnly } = req.body;
@@ -10,19 +10,19 @@ export async function postContact(req, res) {
     return res.success({ message: 'your message has been sent successfully!' });
   }
 
-  await Mail.send({
-    from: `"${name}" <${email}>`,
-    to: EMAIL.EMAIL,
-    subject: `Contact Request from ${email}`,
-    message: `
-      <div>
-        <p><span style="font-weight: bold;">Name:</span> ${name}</p>
-        <p><span style="font-weight: bold;">Email:</span> ${email}</p>
-        <p><span style="font-weight: bold;">Subject:</span> ${subject}</p>
-        <p><span style="font-weight: bold;">Message:</span> ${message}</p>
-      </div>
-    `,
-  });
+  // await Mail.send({
+  //   from: `"${name}" <${email}>`,
+  //   to: EMAIL.EMAIL,
+  //   subject: `Contact Request from ${email}`,
+  //   message: `
+  //     <div>
+  //       <p><span style="font-weight: bold;">Name:</span> ${name}</p>
+  //       <p><span style="font-weight: bold;">Email:</span> ${email}</p>
+  //       <p><span style="font-weight: bold;">Subject:</span> ${subject}</p>
+  //       <p><span style="font-weight: bold;">Message:</span> ${message}</p>
+  //     </div>
+  //   `,
+  // });
 
   await saveContact({ name, email, subject, message });
 
